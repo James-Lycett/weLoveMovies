90e8852a4bf66881813dc51142d312cc6526ea32
James-Lycett <jrlycett@mail.com>
2023-11-02 18:42:58 -0700

    lots of boilerplate stuff, defined routes in app.js, added some error handling stuff (errorHandler and notFound), started shaping out the file structure

--- src/app.js
+++ src/app.js
@@ -1,5 +1,25 @@
 if (process.env.USER) require("dotenv").config();
+const cors = require("cors")
+const pinoHttp = require("pino-http")
 const express = require("express");
+
 const app = express();
+const moviesRouter = require("./movies/movies.router")
+const reviewsRouter = require("./reviews/reviews.router")
+const theatersRouter = require("./theaters/theaters.router")
+const notFound = require("./errors/notFound")
+const errorHandler = require("./errors/errorHandler")
+
+
+app.use(pinoHttp())
+app.use(cors())
+app.use(express.json())
+
+app.use("/movies", moviesRouter)
+app.use("/reviews", reviewsRouter)
+app.use("theaters", theatersRouter)
+
+app.use(notFound)
+app.use(errorHandler)
 
 module.exports = app;
---
+++ src/movies/movies.router.js
@@ -0,0 +1,6 @@
+const router = require("express").Router({ mergeParams: true })
+
+
+router.route("/")
+
+module.exports = router
