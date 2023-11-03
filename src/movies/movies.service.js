e981a11f9f3c784c9f0f7c2bfd0c10dc01a0d20f
James-Lycett <jrlycett@mail.com>
2023-11-03 12:58:50 -0700

    refactored knex queries in movies.service to use sqlite-friendly groupBy rather than postgres-only distinctOn so the sqlite powered test env would work...

--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -10,19 +10,19 @@ function list() {
 
 function listIsShowing() {
     return knex("movies as m")
-        .join("movies_theaters as mt", "mt.movie_id", "m.movie_id")    
-        .select("*")    
-        .distinctOn("m.movie_id")   
-        .where({ "mt.is_showing": true})
-        .orderBy("m.movie_id")
-}
+      .join("movies_theaters as mt", "mt.movie_id", "m.movie_id")
+      .select("m.*")
+      .where({ "mt.is_showing": true })
+      .groupBy("m.movie_id")
+      .orderBy("m.movie_id");
+  }
 
 function listIsNotShowing() {
     return knex("movies as m")
         .join("movies_theaters as mt", "mt.movie_id", "m.movie_id")        
-        .select("*")
-        .distinctOn("m.movie_id")
-        .where({ is_showing: false})
+        .select("m.*")        
+        .where({ "mt.is_showing": false})
+        .groupBy("m.movie_id")
         .orderBy("m.movie_id")
 }
 
